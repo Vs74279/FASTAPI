cc2a919d43528a3ca89a57b0c8ac75eb6ffa593e
vikash kumar singh <drv74279@gmail.com>
2024-04-12 15:25:55 +0530

    add bolg file and id

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-hello
+from fastapi import FastAPI
+
+app= FastAPI()
+
+@app.get("/")
+def index():
+    return {"data": "vikash"}
+
+@app.get("/blog")
+def blog():
+    return {"data": "blog page"}  
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}   
+
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": id}   
+
+
+@app.get("/blog/{id}/comment")
+def show(id):
+    return {"data": {"1","2"}}         
